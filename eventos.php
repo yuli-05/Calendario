10a6ca2602891860db3af1081630f9f16b791515
yuli-05 <51242400+yuli-05@users.noreply.github.com>
2019-11-30 16:57:41 -0600

    Add files via upload
    eventos de la base de datos

--- eventos.php
+++ eventos.php
@@ -1,10 +1,11 @@
 <?php
 header('Content-Type: application/json');
 //le puse sistema por que es el nombre de mi base de datos :)
-$pdo=new PDO("mysql:dbname=sistema;host=127.0.0.1","root","root");
+$pdo=new PDO("mysql:dbname=sistema;localhost","root","");
 
 // Seleccionar los eventos del calendario 
 $sentenciaSQL= $pdo->prepare("SELECT * FROM eventos");
+
 $sentenciaSQL->execute();
 
 $resultado= $sentenciaSQL->fetchALL(PDO::FETCH_ASSOC);
